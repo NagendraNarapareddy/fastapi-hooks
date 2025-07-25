6b40c9bd7b022b1ef9728905ba3a9d23d336d92a
Nagendra <nagendranarapareddy@gmail.com>
2025-07-25 22:17:24 +0530

    feat(security): added store_jwt_token and get_jwt_token ✅

--- fastapi_hooks/security/use_jwt.py
+++ fastapi_hooks/security/use_jwt.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
+from fastapi import Request,Response
 from jose.constants import ALGORITHMS
 
 ALGORITHM = "HS256"
@@ -11,7 +12,7 @@ class JWTTokenError(Exception):
     pass
 
 
-def generate_token( data: dict, secret_key: str, algorithm: str = ALGORITHM, expires_delta: timedelta = None) -> str:
+def generate_jwt_token( data: dict, secret_key: str, algorithm: str = ALGORITHM, expires_delta: timedelta = None) -> str:
     
     if not data or not isinstance(data, dict):
         raise JWTTokenError("Payload 'data' must be a non-empty dictionary.")
@@ -35,4 +36,28 @@ def generate_token( data: dict, secret_key: str, algorithm: str = ALGORITHM, exp
         return jwt.encode(payload, secret_key, algorithm=algorithm)
     except JWTError as e:
         raise JWTTokenError(f"JWT encoding failed: {e}")
+
+def store_jwt_token(request:Request,response:Response,token: str,max_age: int = 1800)-> None:
+    try:
+        if not token or not isinstance(token, str):
+            raise JWTTokenError("Token must be a non-empty string.")
+    
+        response.set_cookie(
+            key="jwt",
+            value=token,
+            httponly=True,
+            max_age=max_age,
+            samesite="Lax",
+            secure=True
+        )
+        
+    except Exception as e:
+        raise JWTTokenError(f"Failed to store JWT token in cookie: {e}")
+
+def get_jwt_token(request: Request, response: Response, data:dict)->str:
+    
+    token=generate_jwt_token()
+    store_jwt_token(request,response,data)
+    return {"jwt_token":token}
+
 
